54197de968cc89de780c345218ac51b2a2d70417
Maria Senina <maria.senina@confirmit.com>
2017-03-30 11:33:23 +0300

    travis fix

--- gulpfile.js
+++ gulpfile.js
@@ -27,7 +27,7 @@ gulp.task("style", function() {
     .pipe(minify())
     .pipe(rename("style.min.css"))
     .pipe(gulp.dest("build/css"));
-   // .pipe(server.stream());
+    // .pipe(server.stream());
 });
 
 gulp.task("images", function() {
